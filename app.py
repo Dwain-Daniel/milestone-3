4024529452de51598be2f5f66ed9f0fc225ba14e
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-04-30 00:10:05 +0000

    Log out functionality created. Webpages still not loading correctly

--- app.py
+++ app.py
@@ -74,7 +74,18 @@ def login():
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
+    
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    
     return render_template("profile.html", username=username)
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
